7701ce20a1478ebd20007ffdfdb209e08c9f366c
Cihad Horuzoglu <cihadhoruzoglu@gmail.com>
2014-10-24 12:30:50 +0200

    Home page added with categories

--- www/js/app.js
+++ www/js/app.js
@@ -6,67 +6,100 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
+    .factory('api', function($http) {
+        var config = {
+            baseUrl : "https://api.paris.fr/api/data/1.0/",
+            token   : "39126f7a669cc58f7814e1f2f2d7b26fdee0e2617c764c225f62aed434df8003",
+
+        };
+
+        return {
+            getCategories: function(success) {
+                $http({
+                    url: config.baseUrl + 'Equipements/get_categories/',
+                    method: "GET",
+                    params: {
+                        token: config.token
+                    }
+                })
+                    .success(function(data) {
+                        success(data);
+                    });
+            }
         }
-      }
     })
 
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.home', {
+                url: "/home",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/home.html",
+                        controller: "HomeCtrl"
+                    }
+                }
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/search.html"
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/home');
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
 
